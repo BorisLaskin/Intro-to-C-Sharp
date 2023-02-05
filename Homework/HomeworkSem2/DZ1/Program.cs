68141772b04aef344934fd00f133fe57a4418756
BorisLaskin <varislaskin@list.ru>
2023-02-05 16:02:15 +0300

    sem2_dz_1

--- Homework/HomeworkSem2/DZ1/Program.cs
+++ Homework/HomeworkSem2/DZ1/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа. Не использовать строки для расчета.
+int InputInt(string massage)
+{
+    System.Console.WriteLine(massage);
+    int x =Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+while(true)
+{
+    int A=InputInt("Введите трехзначное число");
+    if(A>100 && A<1000)
+    {
+        System.Console.WriteLine($"Вторая цифра числа {A} - это {(A%100)/10}");
+        break;
+    }
+    else System.Console.WriteLine("число нетрехзначное");
+}
