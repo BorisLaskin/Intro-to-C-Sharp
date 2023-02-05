40b90fd385e2f7164ff42a2f4fb31d0aef0d2ff3
BorisLaskin <varislaskin@list.ru>
2023-02-05 16:20:46 +0300

    Sem2_DZ_done

--- Homework/HomeworkSem2/DZ3/Program.cs
+++ Homework/HomeworkSem2/DZ3/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int InputInt(string massage)
+{
+    System.Console.WriteLine(massage);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+int A = InputInt("Введите число->");
+if (A > 0 && A < 8)
+{
+    if (A == 6 || A == 7)
+        System.Console.WriteLine("да");
+    else
+        System.Console.WriteLine("нет");
+}
+else System.Console.WriteLine("не является днем недели");
