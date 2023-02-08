746f2bffc462dc36318a9a42042612d9ed01d709
BorisLaskin <varislaskin@list.ru>
2023-02-08 22:51:30 +0300

    lections 1_2 added

---
+++ Examples/lection2/Task4_RandArrayGen/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] collection)
+{
+    int ArrayLength = collection.Length;
+    int index = 0;
+    while(index < ArrayLength)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] collection)
+{
+    int count = collection.Length;
+    int pos = 0;
+    while(pos<count)
+    {
+        System.Console.WriteLine(collection[pos]);
+        pos++;
+    }
+}
+int IndexOf(int[] collection,int find)
+{
+    int Num = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < Num)
+    {
+        if(collection[index]==find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+int find =4;
+int pos = IndexOf(array,find);
+System.Console.WriteLine($"{find} at {pos}");
