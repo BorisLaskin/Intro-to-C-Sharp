e24df9e28a094691a07e96ce0923a4a331248809
BorisLaskin <varislaskin@list.ru>
2023-02-11 13:58:20 +0300

    Sem3_DZ_2,3

--- Homework/HomeworkSem3/DZ1/Program.cs
+++ Homework/HomeworkSem3/DZ1/Program.cs
@@ -4,3 +4,18 @@ int Promt(string message)
     System.Console.WriteLine(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+int GetNumberRank(int number)
+{
+    int Rank = 1;
+    while (number > 10)
+    {
+        number / 10;
+        Rank++;
+    }
+    return Rank;
+}
+int testNumber = Promt("Введите положительное число");
+if(testNumber>0)
+{
+    System.Console.WriteLine(GetNumberRank(testNumber));
+}
