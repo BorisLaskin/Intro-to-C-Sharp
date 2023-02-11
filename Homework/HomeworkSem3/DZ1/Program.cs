dccf5bfb017cbc430a25f745c51804f6ecc78b14
BorisLaskin <varislaskin@list.ru>
2023-02-11 14:15:33 +0300

    Sem3_DZ_done

--- Homework/HomeworkSem3/DZ1/Program.cs
+++ Homework/HomeworkSem3/DZ1/Program.cs
@@ -9,13 +9,34 @@ int GetNumberRank(int number)
     int Rank = 1;
     while (number > 10)
     {
-        number / 10;
+        number = number / 10;
         Rank++;
     }
     return Rank;
 }
 int testNumber = Promt("Введите положительное число");
-if(testNumber>0)
+if (testNumber > 0)
 {
-    System.Console.WriteLine(GetNumberRank(testNumber));
+    int numberRank = GetNumberRank(testNumber);
+    bool isPolindrome = true;
+    while (numberRank > 1)
+    {
+        var a=testNumber % 10;
+        var b=testNumber / Convert.ToInt32(Math.Pow(10, numberRank-1));
+        if (a == b)
+        {
+            testNumber = testNumber % Convert.ToInt32(Math.Pow(10, numberRank-1));
+            testNumber = testNumber / 10;
+            numberRank=numberRank-2;
+        }
+        else
+        {
+            isPolindrome = false;
+            break;
+        }
+    }
+    if (isPolindrome)
+        System.Console.WriteLine("это полиндром");
+    else
+    System.Console.WriteLine("нет, это не полиндром");
 }
