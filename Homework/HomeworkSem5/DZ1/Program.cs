00ebe088b98c0d3f0b5ebe60b4685ee5480f8a24
BorisLaskin <varislaskin@list.ru>
2023-02-18 13:58:52 +0300

    DZ5

--- Homework/HomeworkSem5/DZ1/Program.cs
+++ Homework/HomeworkSem5/DZ1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве. [345, 897, 568, 234] -> 2
+
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] CreateRandomArray(int arraySize, int minValue, int maxValue)
+{
+    int[] numericArray = new int[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        numericArray.SetValue(new Random().Next(minValue, maxValue + 1), i);
+    }
+    return numericArray;
+}
+void PrintArray(int[] numericArray)
+{
+    for (int i = 0; i < numericArray.Length; i++)
+    {
+        System.Console.Write($"{numericArray[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int CountEven(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item % 2 == 0)
+            count++;
+    }
+    return count;
+}
+int min = 100;
+int max = 999;
+int number = Promt("Введите число элементов массива >");
+if (number > 0)
+{
+    int[] array = CreateRandomArray(number, min, max);
+    PrintArray(array);
+    System.Console.WriteLine($"Количество четных элементов в массиве равно {CountEven(array)}");
+}
+else
+{
+    System.Console.WriteLine("Число элементов должно быть положительным!");
+}
--- Homework/HomeworkSem5/DZ2/Program.cs
+++ Homework/HomeworkSem5/DZ2/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] CreateRandomArray(int arraySize, int minValue, int maxValue)
+{
+    int[] numericArray = new int[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        numericArray.SetValue(new Random().Next(minValue, maxValue + 1), i);
+    }
+    return numericArray;
+}
+void PrintArray(int[] numericArray)
+{
+    for (int i = 0; i < numericArray.Length; i++)
+    {
+        System.Console.Write($"{numericArray[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int UnevenPosSum(int[] array)
+{
+    int sum=0;
+    int i=0;
+    while(i<array.Length)
+    {
+        sum+=array[i];
+        i+=2;
+    }
+    return sum;
+}
+
+// Основное тело скрипта
+int userArrayLength = Promt("Введите длину генерируемого массива (Положительное число) - >");
+if (userArrayLength > 0)
+{
+    int userMinValue = Promt("Введите минимальное значение (Целое число) - >");
+    int userMaxValue = Promt("Введите максимальное значение (Целое число) - >");
+
+    if (userMinValue>userMaxValue)
+    {
+        System.Console.WriteLine("Вы перепутали минимальное и максимальное значения");
+        int temp = userMinValue;
+        userMinValue = userMaxValue;
+        userMaxValue = temp;
+
+    }
+    int[] userArray=CreateRandomArray(userArrayLength, userMinValue, userMaxValue);
+    PrintArray(userArray);
+    System.Console.WriteLine($"Сумма элементов на нечетных позициях {UnevenPosSum(userArray)}");
+}
+else
+    System.Console.WriteLine("Длина массива должна быть положительной");
