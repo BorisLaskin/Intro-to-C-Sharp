cdbea5c7e65a2bd8156360839eb79a7070412b2b
BorisLaskin <varislaskin@list.ru>
2023-02-19 13:55:48 +0300

    Sem6_DZ

---
+++ Homework/HomeworkSem6/DZ1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int[] IntMassPromt(string message)
+{
+    System.Console.WriteLine(message);
+    string[] strings = Console.ReadLine().Split();
+    int[] nums = new int[strings.Length];
+    for (int i = 0; i < strings.Length; i++)
+        nums[i] = Convert.ToInt32(strings[i]);
+    return nums;
+}
+
+int CountPositive(int[] array)
+{
+    int result = 0;
+    foreach (int item in array)
+    {
+        if (item > 0)
+            result++;
+    }
+    return result;
+}
+// Script text
+int[] array = IntMassPromt("Введите M чисел через пробел");
+System.Console.WriteLine($"В указанном массиве {CountPositive(array)} элементов больше нуля");
---
+++ Homework/HomeworkSem6/DZ2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями y = k1 *
+// x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+int[] IntMassPromt(string message)
+{
+    System.Console.WriteLine(message);
+    string[] strings = Console.ReadLine().Split();
+    int[] nums = new int[strings.Length];
+    for (int i = 0; i < strings.Length; i++)
+        nums[i] = Convert.ToInt32(strings[i]);
+    return nums;
+}
+double[] Peresechenie(int[] paramOne,int[] paramTwo)
+{
+    double[] result=new double[2];
+    result[0]=(paramTwo[1]-paramOne[1])/(paramOne[0]-paramTwo[0]);
+    result[1]=paramOne[0]*result[0]+paramOne[1];
+
+    return result;
+}
+//Script text
+int[] arrayOne = IntMassPromt("Введите первое коэффициенты уравнения");
+int[] arrayTwo = IntMassPromt("Введите первое коэффициенты уравнения");
