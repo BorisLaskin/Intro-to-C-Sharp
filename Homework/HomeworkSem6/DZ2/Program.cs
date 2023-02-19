a5fffd438e1a80ed643354df4b2fc2212fd37f21
BorisLaskin <varislaskin@list.ru>
2023-02-19 14:36:17 +0300

    Sem6_DZ_DoneM

--- Homework/HomeworkSem6/DZ2/Program.cs
+++ Homework/HomeworkSem6/DZ2/Program.cs
@@ -4,18 +4,25 @@
 // пользователем.
 double[] DoubleMassPromt(string message)
 {
-    System.Console.WriteLine(message);
-    string[] strings = Console.ReadLine().Split();
-    double[] nums = new double[strings.Length];
-    for (int i = 0; i < strings.Length; i++)
-        nums[i] = Convert.ToDouble(strings[i]);
-    return nums;
+    while (true)
+    {
+        System.Console.WriteLine(message);
+        string[] strings = Console.ReadLine().Split();
+        if (strings.Length == 2)
+        {
+            double[] nums = new double[strings.Length];
+            for (int i = 0; i < strings.Length; i++)
+                nums[i] = Convert.ToDouble(strings[i]);
+            return nums;
+        }
+        System.Console.WriteLine("Вы ввели не 2 числа.Попробуй снова.");
+    }
 }
-double[] Peresechenie(double[] paramOne,double[] paramTwo)
+double[] Peresechenie(double[] paramOne, double[] paramTwo)
 {
-    double[] result=new double[2];
-    result[0]=(paramTwo[0]-paramOne[0])/(paramOne[1]-paramTwo[1]);
-    result[1]=paramOne[1]*result[0]+paramOne[0];
+    double[] result = new double[2];
+    result[0] = (paramTwo[0] - paramOne[0]) / (paramOne[1] - paramTwo[1]);
+    result[1] = paramOne[1] * result[0] + paramOne[0];
 
     return result;
 }
@@ -30,5 +37,5 @@ void PrintArray(double[] numericArray)
 //Script text
 double[] arrayOne = DoubleMassPromt("Введите b k коэффициенты первого уравнения через пробел >");
 double[] arrayTwo = DoubleMassPromt("Введите b k коэффициенты второго уравнения через пробел >");
-double[] result = Peresechenie(arrayOne,arrayTwo);
+double[] result = Peresechenie(arrayOne, arrayTwo);
 PrintArray(result);
